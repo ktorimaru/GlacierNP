fa8bbf997110e9a583dd67e875d8371442bd1577
Ken <ken@torimaru.com>
2019-12-10 08:55:54 -0800

    Initial Commit

---
+++ GlacierNP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlacierNP
+//
+//  Created by Ken Torimaru on 12/10/19.
+//  Copyright © 2019 Torimaru & Williamson, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
